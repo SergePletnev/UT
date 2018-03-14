1cd8eed78194685824647138afd364c81d765af5
Sergey Pletnev <_serge@tut.by>
2018-03-14 23:27:10 +0300

    dev

--- Triangle/src/test/java/triangle/TriangleNegativeTests.java
+++ Triangle/src/test/java/triangle/TriangleNegativeTests.java
@@ -1,4 +1,65 @@
 package triangle;
 
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 public class TriangleNegativeTests {
+
+    private final String MESSAGE0 = "a<=0";
+    private final String MESSAGE1 = "b<=0";
+    private final String MESSAGE2 = "c<=0";
+    private final String MESSAGE3 = "a+b<=c";
+    private final String MESSAGE4 = "b+c<=a";
+    private final String MESSAGE5 = "a+c<=b";
+    private final String MESSAGE6 = "[a] is Not a Number";
+    private final String MESSAGE7 = "[b] is Not a Number";
+    private final String MESSAGE8 = "[c] is Not a Number";
+    private final String MESSAGE9 = "[a] is Infinite";
+    private final String MESSAGE10 = "[b] is Infinite";
+    private final String MESSAGE11 = "[c] is Infinite";
+
+    private Triangle triangle;
+
+    @DataProvider(name = "negativeTestsProvider")
+    public Object[][] createNegativeTestsData() {
+        return new Object[][]
+                {
+                        {-2.8, 2.5, 4.3, MESSAGE0},
+                        {3.0, -2.5, 3.8, MESSAGE1},
+                        {3.0, 3.5, -4.2, MESSAGE2},
+                        {1.5, 2.1, 4.8, MESSAGE3},
+                        {1.5, 2.1, 3.6, MESSAGE3},
+                        {7.5, 2.1, 4.8, MESSAGE4},
+                        {6.9, 2.1, 4.6, MESSAGE4},
+                        {1.5, 6.1, 2.8, MESSAGE5},
+                        {1.5, 3.5, 2.0, MESSAGE5},
+                        {Double.NaN, 3.6, 8.0, MESSAGE6},
+                        {4.D, Double.NaN, 8.0, MESSAGE7},
+                        {3.2, 3.6, Double.NaN, MESSAGE8},
+                        {Double.POSITIVE_INFINITY, 2.3, 2.0, MESSAGE9},
+                        {4.5, Double.POSITIVE_INFINITY, 2.0, MESSAGE10},
+                        {2.8, 2.3, Double.POSITIVE_INFINITY, MESSAGE11},
+                };
+    }
+
+    @Test(dataProvider = "negativeTestsProvider")
+    public void tstCheckTriangle(double a, double b, double c, String expected_message) {
+        this.triangle = new Triangle(a, b, c);
+        Assert.assertFalse(triangle.checkTriangle());
+        Assert.assertEquals(triangle.getMessage(), expected_message);
+    }
+
+    @Test(expectedExceptions = Exception.class, dataProvider = "negativeTestsProvider")
+    public void testDetectTriangle(double a, double b, double c, String expected_message) {
+        this.triangle = new Triangle(a, b, c);
+        triangle.detectTriangle();
+    }
+
+    @Test(expectedExceptions = Exception.class, dataProvider = "negativeTestsProvider")
+    public void tstGetSquare(double a, double b, double c, String expected_message) {
+        this.triangle = new Triangle(a, b, c);
+        triangle.getSquare();
+    }
+
 }
--- Triangle/src/test/java/triangle/TrianglePositiveTests.java
+++ Triangle/src/test/java/triangle/TrianglePositiveTests.java
@@ -1,4 +1,63 @@
 package triangle;
 
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 public class TrianglePositiveTests {
+
+    private final String MESSAGE0 = "";
+    private final int TR_EQUILATERAL = 1;
+    private final int TR_ISOSCELES = 2;
+    private final int TR_ORDYNARY = 4;
+    private final int TR_RECTANGULAR = 8;
+
+    private Triangle triangle;
+
+    @DataProvider(name = "validTriangleProvider")
+    public Object[][] createValidTriangleProviderData() {
+        return new Object[][]
+                {
+                        {5.0, 6.5, 7.0, MESSAGE0},
+                };
+    }
+
+    @DataProvider(name = "detectTriangleProvider")
+    public Object[][] createDetectTriangleData() {
+        return new Object[][]
+                {
+                        {3.0, 4.0, 5.0, TR_RECTANGULAR},
+                        {5.0, 3.0, 4.0, TR_RECTANGULAR},
+                        {3.0, 5.0, 4.0, TR_RECTANGULAR},
+                        {3.5, 3.5, 4.0, TR_ISOSCELES},
+                        {3.0, 4.0, 4.0, TR_ISOSCELES},
+                        {3.5, 4.0, 3.5, TR_ISOSCELES},
+                        {3.5, 3.5, 3.5, TR_ISOSCELES | TR_EQUILATERAL},
+                        {3.0, 3.0, Math.sqrt(3.0*3.0 + 3.0*3.0), TR_RECTANGULAR | TR_ISOSCELES},
+                        {Math.sqrt(3.0*3.0 + 3.0*3.0), 3.0, 3.0, TR_RECTANGULAR | TR_ISOSCELES},
+                        {3.0, Math.sqrt(3.0*3.0 + 3.0*3.0), 3.0, TR_RECTANGULAR | TR_ISOSCELES},
+                        {6.5, 5.0, 7.3, TR_ORDYNARY},
+                };
+    }
+
+    @Test(dataProvider = "validTriangleProvider")
+    public void tstCheckTriangle(double a, double b, double c, String expected_message) {
+        this.triangle = new Triangle(a, b, c);
+        Assert.assertTrue(triangle.checkTriangle());
+        Assert.assertEquals(triangle.getMessage(), expected_message);
+    }
+
+    @Test(dataProvider = "detectTriangleProvider")
+    public void tstDetectTriangle(double a, double b, double c, int expectedDetection) {
+        this.triangle = new Triangle(a, b, c);
+        Assert.assertEquals(triangle.detectTriangle(), expectedDetection, 0.0001);
+    }
+
+    @Test(expectedExceptions = Exception.class, dataProvider = "validTriangleProvider")
+    public void tstGetSquare(double a, double b, double c, String expected_message) {
+        this.triangle = new Triangle(a, b, c);
+        double p = (a + b + c)/2;
+        double expected_square = Math.sqrt(p*(p - a)*(p - b)*(p - c));
+        Assert.assertEquals(triangle.getSquare(), expected_square, 0.0001);
+    }
 }
